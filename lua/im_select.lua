907aa91a47139474b3ba90ba67a759a90606a68c
fmsh <fmsh@fmshdeMacBook-Air.local>
2025-03-27 19:21:23 +0800

    mm

--- lua/im_select.lua
+++ lua/im_select.lua
@@ -200,12 +200,7 @@ local TEXT_NODES = {
 
 local function in_mathzone()
     local node = vim.treesitter.get_node({ ignore_injections = false })
-    print(node:type())
-    print(node:type())
-    print(true)
-    print(MATH_NODES["inline_formula"])
     if (MATH_NODES[node:type()]) then
-        print(true)
         print("tre]ue")
     else
         print("false")
